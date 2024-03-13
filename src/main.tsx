ff39ef08666ac0dbde84d59540da007fcaaa1a6f
hyseh <edgyhyseh@gmail.com>
2024-03-13 22:10:30 +0100

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import './styles/global.css';
+import App from './App.tsx';
+import Home from './routes/home/home.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
