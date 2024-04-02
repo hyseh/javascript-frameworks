651c968f6559f0a1963d5369b73e10cd4a663c17
hyseh <edgyhyseh@gmail.com>
2024-04-02 17:30:49 +0200

    Updated Contact page and added styling

--- src/routes/Contact/index.tsx
+++ src/routes/Contact/index.tsx
@@ -1,9 +1,23 @@
+import styles from './style.module.css';
 import ContactForm from '../../components/Forms/ContactForm';
+import ContactDetails from '../../components/Contact/ContactDetails';
+import ContactFaq from '../../components/Contact/ContactFaq';
 
 export default function Contact() {
   return (
     <section>
-      <ContactForm />
+      <div className={styles.grid}>
+        <div className={styles.container}>
+          <h2>Contact us</h2>
+          <ContactDetails />
+          <h2>Frequently asked questions</h2>
+          <ContactFaq />
+        </div>
+        <div className={styles.container}>
+          <h2>Send us a message</h2>
+          <ContactForm />
+        </div>
+      </div>
     </section>
   );
 }
