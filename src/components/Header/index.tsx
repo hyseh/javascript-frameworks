190956029325d7a07578a0d2fa8f82067e6143ca
hyseh <edgyhyseh@gmail.com>
2024-04-04 23:10:15 +0200

    Updated Header component and fixed styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,41 @@
 import { useState } from 'react';
 import styles from './style.module.css';
+import { Menu } from 'react-feather';
 import Nav from './Nav';
+import CartButton from '../Cart/CartButton';
+import CartMenu from '../Cart/CartMenu';
 
 export default function Header() {
-  const [isActive, setIsActive] = useState(false);
+  const [isNavActive, setIsNavActive] = useState(false);
+  const [isCartActive, setIsCartActive] = useState(false);
 
   return (
     <header>
       <div className={styles.header}>
-        <div>
-          <a href="/" className={styles.logo}>
-            Header
-          </a>
+        <div className={styles.logo}>
+          <a href="/">Header</a>
         </div>
-        <button
-          className={styles.button}
-          onClick={() => {
-            setIsActive(!isActive);
-          }}
-        >
-          Menu
-        </button>
-        <Nav isActive={isActive} setIsActive={setIsActive} />
+
+        <div className={styles.mobile}>
+          <CartButton isActive={isCartActive} setIsActive={setIsCartActive} />
+          <button
+            className={styles.menuButton}
+            onClick={() => {
+              setIsNavActive(!isNavActive);
+            }}
+          >
+            <span className="sr-only">Open navigation menu</span>
+            <Menu />
+          </button>
+        </div>
+
+        <Nav isActive={isNavActive} setIsActive={setIsNavActive} />
+
+        <div className={styles.desktop}>
+          <CartButton isActive={isCartActive} setIsActive={setIsCartActive} />
+        </div>
+
+        {isCartActive && <CartMenu setIsActive={setIsCartActive} />}
       </div>
     </header>
   );
