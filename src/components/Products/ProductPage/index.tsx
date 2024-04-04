bc8ce5d6d21196037f612b97ed190c5b0a4208ab
hyseh <edgyhyseh@gmail.com>
2024-04-04 23:06:25 +0200

    Added add to cart button

--- src/components/Products/ProductPage/index.tsx
+++ src/components/Products/ProductPage/index.tsx
@@ -1,15 +1,17 @@
 import styles from './style.module.css';
-import CartButton from '../../Buttons/CartButton';
+import { useCartStore } from '../../../hooks/store';
 import ProductDiscount from './ProductDiscount';
 import ProductImage from './ProductImage';
 import ProductPricing from './ProductPricing';
 import ProductReviews from './ProductReviews';
 import ProductRating from './ProductRating';
+import PrimaryButton from '../../Buttons/PrimaryButton';
 
 export default function ProductPage({ product }) {
   const { title, description, price, discountedPrice, image, rating, reviews } =
     product;
   const discount = discountedPrice < price;
+  const addProduct = useCartStore((state) => state.addProduct);
 
   return (
     <div className={styles.grid}>
@@ -24,6 +26,9 @@ export default function ProductPage({ product }) {
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>{description}</p>
         <ProductPricing price={price} discountedPrice={discountedPrice} />
+        <PrimaryButton onClick={() => addProduct(product)}>
+          Add to cart
+        </PrimaryButton>
       </div>
       {reviews.length > 0 && <ProductReviews reviews={reviews} />}
     </div>
