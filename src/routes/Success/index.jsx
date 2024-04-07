b0afc8c83f20a05050a559990af195b414193ffb
hyseh <edgyhyseh@gmail.com>
2024-04-07 16:29:05 +0200

    Added success page content and styling

--- src/routes/Success/index.jsx
+++ src/routes/Success/index.jsx
@@ -1,5 +1,33 @@
+import { Link } from 'react-router-dom';
 import styles from './style.module.css';
+import { CheckCircle } from 'react-feather';
 
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <section>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.icon}>
+            <CheckCircle size={120} />
+          </div>
+          <p className={styles.heading}>Thank you for your order!</p>
+          <p className={styles.paragraph}>
+            <span>Your order number is: 833 855 888 333 555</span>
+            <span>
+              A order confirmation with details was sent to your email.
+            </span>
+          </p>
+          <Link
+            to="/"
+            className={styles.continueButton}
+            onClick={() => {
+              setIsActive(false);
+            }}
+          >
+            Continue Shopping
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
