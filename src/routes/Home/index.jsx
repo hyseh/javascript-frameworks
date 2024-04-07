88ea48a2e4764e1f46feef8f4e115dd3e693651a
hyseh <edgyhyseh@gmail.com>
2024-04-07 13:52:50 +0200

    Added search and updated styling

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import styles from './style.module.css';
 import ProductCard from '../../components/Products/ProductCard';
+import SearchForm from '../../components/Forms/SearchForm';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     async function getData() {
@@ -27,30 +29,53 @@ export default function Home() {
     getData();
   }, []);
 
+  const filteredProducts = products.filter((product) => {
+    if (query) {
+      return product.title.toLowerCase().includes(query.toLowerCase());
+    }
+    return true;
+  });
+
   return (
     <>
       {error && <div>{error}</div>}
       {loading && <div>Loading...</div>}
       {products && (
-        <section>
-          <div className={styles.grid}>
-            {products.map(
-              ({ id, title, description, price, discountedPrice, image }) => {
-                return (
-                  <ProductCard
-                    key={id}
-                    id={id}
-                    title={title}
-                    description={description}
-                    price={price}
-                    discountedPrice={discountedPrice}
-                    image={image}
-                  />
-                );
-              }
-            )}
-          </div>
-        </section>
+        <>
+          <section>
+            <div className={styles.container}>
+              <SearchForm value={query} onChange={setQuery} />
+            </div>
+          </section>
+          <section>
+            <div className={styles.container}>
+              <div className={styles.grid}>
+                {filteredProducts.map(
+                  ({
+                    id,
+                    title,
+                    description,
+                    price,
+                    discountedPrice,
+                    image,
+                  }) => {
+                    return (
+                      <ProductCard
+                        key={id}
+                        id={id}
+                        title={title}
+                        description={description}
+                        price={price}
+                        discountedPrice={discountedPrice}
+                        image={image}
+                      />
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </section>
+        </>
       )}
     </>
   );
